ee82c364f0d2cc5a3e0f3b65cc277d7de72c557d
Juan Sebastian Rodriguez <jsrb48@gmail.com>
2022-10-25 15:40:25 -0500

    Creación de entorno de trabajo con herramientas configuradas - Inicializar el servidor y configuración de Nodemon(motor de verificacion de cambios)

---
+++ back/server.js
@@ -0,0 +1,8 @@
+const app = require("./app")
+
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+const server=app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
