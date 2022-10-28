ba14d2a6762b89b2487dffb390b2bde4007ab971
Manuel <juanzatch17@hotmail.com>
2022-10-28 17:54:49 -0500

    Se añade carrito y rooteo

---
+++ front/src/components/back/Data.jsx
@@ -0,0 +1,29 @@
+const data = {
+    productItems:[
+    {
+        id: '1',
+        name: "Panzeroti",
+        price: 20000,
+        image: "/images/productos/panzeroti.png",
+    },
+    {
+        id: '2',
+        name: "Raviolis",
+        price: 22000,
+        image: "/images/productos/raviolis.png",
+    },
+    {
+        id: '3',
+        name: "Tallarines",
+        price: 25000,
+        image: "/images/productos/tallarines.png",
+    },
+    {
+        id: '4',
+        name: "Tortelini",
+        price: 26000,
+        image: "/images/productos/tortelini.png",
+    },
+]}
+
+export default data; 
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
@@ -6,7 +7,9 @@ export const Header = () => {
         <nav className='navbar row'>
             <div className='col-12 col-md-3'>
                 <div className='navbar-brand'>
-                    <img src='./images/logo.png' alt='Restaurante Delicias Italianas Logo' width="100%" height="100%"></img>
+                    <Link className='nav-link active' to='/'>
+                        <img src='./images/logo.png' alt='Restaurante Delicias Italianas Logo' width="100%" height="100%"></img>
+                    </Link>
                 </div>
             </div>
 
@@ -16,7 +19,7 @@ export const Header = () => {
                         type="text"
                         id="search_field"
                         class="form-control"
-                        placeholder='Que deseas ordenar hoy?'
+                        placeholder='¿Que deseas ordenar hoy?'
                         ></input>
                         <div className='input-group-append'>
                             <button id='search-btn' class='btn'>
@@ -25,12 +28,25 @@ export const Header = () => {
                         </div>
                 </div>
             </div>
+
+            
             <div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
-                <span><button className='btn' id="login_btn">Inicie Sesión</button></span>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <i class='fa fa-shopping-cart fa-2x text-white' aria-hidden="true"></i>
-                <span className='ml-1' id='cart_count'>2</span>
+                
+                <Link className='nav-link' to='/login'>
+                    <span><button className='btn' id="login_btn">Inicie Sesión</button></span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </Link>
+                           
+                
+                <Link className='nav-link' to='/cart'>
+                    <i class='fa fa-shopping-cart fa-2x text-white' aria-hidden="true"></i>
+                    <span className='ml-1' id='cart_count'>0</span>
+                </Link>
+                
             </div>
+
+
+            
         </nav>
     </Fragment>
   )
